84f8f94e0500fd16f903edc3c9969acd08ac5bc1
Geza Kovacs <geza0kovacs@gmail.com>
2020-04-07 02:59:30 -0700

    fixed dictionary

--- data/convert.py
+++ data/convert.py
@@ -13,7 +13,8 @@ for line in open('cangjie5.dict.yaml'):
   parts = line.split('\t')
   char = parts[0]
   #char_simp = opencc.convert(word, config='t2s.json')
-  char_to_cangjie[char] = []
+  if char not in char_to_cangjie:
+    char_to_cangjie[char] = []
   for cangjie_code in parts[1:]:
     if "'" in cangjie_code:
       continue
@@ -47,6 +48,35 @@ min_phrase_weight: 100
 ...
 '''
 
+def get_all_cangjie_for_each_char(char_list):
+  output = []
+  for x in char_list:
+    cangjie_list = char_to_cangjie.get(x, [])
+    first_chars = list(set([x[0] for x in cangjie_list]))
+    if len(first_chars) > 1:
+      if len([x for x in first_chars if x != 'x']) > 0:
+        first_chars = [x for x in first_chars if x != 'x']
+    output.append(first_chars)
+  return output
+
+def get_all_cangjie_combo_list(char_list):
+  cangjie_for_each_char = get_all_cangjie_for_each_char(char_list)
+  #print(cangjie_for_each_char)
+  mlen = len(cangjie_for_each_char)
+  def subr(start_idx=0):
+    if start_idx >= mlen:
+      return []
+    if start_idx == mlen - 1:
+      return [[x] for x in cangjie_for_each_char[start_idx]]
+    else:
+      output = []
+      remainder = subr(start_idx + 1)
+      for x in cangjie_for_each_char[start_idx]:
+        for y in remainder:
+          output.append([x] + y)
+      return output
+  return subr(0)
+
 outfile = open('../terra_pinyin.cangjie5.dict.yaml', 'wt')
 print(header_text, file=outfile)
 num_lines = 0
@@ -69,24 +99,20 @@ for line in open('../terra_pinyin.dict.yaml'):
   #chars_simp = chars
   chars_orig_simp = opencc.convert(chars_orig, config='t2s.json')
   chars_simp = chars_orig_simp.replace('·', '')
-  cangjie_list = [char_to_cangjie.get(x, [[None]])[0][0] for x in chars]
-  cangjie_simp_list = [char_to_cangjie.get(x, [[None]])[0][0] for x in chars_simp]
-  output_trad = True
-  if None in cangjie_list:
-    output_trad = False
-  output_simp = True
-  if None in cangjie_simp_list:
-    output_simp = False
-  if cangjie_simp_list == cangjie_list:
-    output_simp = False
-  if output_trad:
+  cangjie_combo_list = get_all_cangjie_combo_list(chars)
+  cangjie_simp_combo_list = get_all_cangjie_combo_list(chars_simp)
+  for cangjie_list in cangjie_combo_list:
+    if None in cangjie_list:
+      continue
     readings_str = ' '.join([pinyin_base(pinyin)+pinyin_tone(pinyin)+cangjie_letter_to_hanzi(cangjie) for pinyin,cangjie in zip(readings_list, cangjie_list)])
     outline = chars_orig + '\t' + readings_str
     if len(parts) > 2:
       outline += '\t' + ('\t'.join(parts[2:]))
     print(outline, file=outfile)
-  if output_simp:
-    readings_str = ' '.join([pinyin_base(pinyin)+pinyin_tone(pinyin)+cangjie_letter_to_hanzi(cangjie) for pinyin,cangjie in zip(readings_list, cangjie_simp_list)])
+  for cangjie_list in [x for x in cangjie_simp_combo_list if x not in cangjie_combo_list]:
+    if None in cangjie_list:
+      continue
+    readings_str = ' '.join([pinyin_base(pinyin)+pinyin_tone(pinyin)+cangjie_letter_to_hanzi(cangjie) for pinyin,cangjie in zip(readings_list, cangjie_list)])
     outline = chars_orig + '\t' + readings_str
     if len(parts) > 2:
       outline += '\t' + ('\t'.join(parts[2:]))
